5cec4557ff4eadc9fa0d6de0014e72ff5559739b
Felix <felix@msynthnova.com>
2024-10-01 13:56:37 -0500

    change color when enter and exit normal mode neovim

--- init.lua
+++ init.lua
@@ -20,3 +20,29 @@ keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- Define highlight groups for different modes
+vim.api.nvim_set_hl(0, 'StatusLineNormal', { fg = '#ffffff', bg = '#005f5f' })
+vim.api.nvim_set_hl(0, 'StatusLineInsert', { fg = '#ffffff', bg = '#5f00af' })
+
+-- Set initial color (normal mode)
+vim.api.nvim_set_hl(0, 'StatusLine', { fg = '#ffffff', bg = '#005f5f' })
+
+-- Autocommands to change status line color based on mode
+vim.api.nvim_create_augroup('ModeColor', { clear = true })
+
+vim.api.nvim_create_autocmd('InsertEnter', {
+  group = 'ModeColor',
+  pattern = '*',
+  callback = function()
+    vim.api.nvim_set_hl(0, 'StatusLine', { fg = '#ffffff', bg = '#5f00af' })
+  end,
+})
+
+vim.api.nvim_create_autocmd('InsertLeave', {
+  group = 'ModeColor',
+  pattern = '*',
+  callback = function()
+    vim.api.nvim_set_hl(0, 'StatusLine', { fg = '#ffffff', bg = '#005f5f' })
+  end,
+})
