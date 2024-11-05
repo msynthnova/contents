b49943b765248e58088fbb52ee00cfd2638cc77e
Felix <felix@msynthnova.com>
2024-11-04 19:26:29 -0600

    upate

---
+++ status.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+while true; do
+    # Get CPU usage
+    cpu_usage=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}')
+    
+    # Get RAM usage
+    ram_usage=$(free -m | awk '/^Mem:/{printf "%.2f/%d MB", $3, $2}')
+    
+    # Get battery status
+    battery_status=$(acpi | awk '{print $3, $4}' | tr -d ',')
+    
+    # Get date and time
+    date_time=$(date +"%Y-%m-%d %H:%M:%S")
+
+    # Output the status
+    echo "CPU: $cpu_usage% | RAM: $ram_usage | Battery: $battery_status | $date_time"
+
+    sleep 1
+done
